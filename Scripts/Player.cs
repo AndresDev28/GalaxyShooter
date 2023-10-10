aec39e9330693c3f810dd112bddbe459f4b520c9
AndresDev28 <andresperezandreiev@MacBook-Air-de-Andres.local>
2023-10-10 14:17:55 +0200

    Actualizacionde movimiento de player

--- Scripts/Player.cs
+++ Scripts/Player.cs
@@ -13,6 +13,38 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Movement();
+    }
+    private void Movement()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * Time.deltaTime * speed * horizontalInput);
+
+        float verticalInput = Input.GetAxis("Vertical");
+        transform.Translate(Vector3.up * Time.deltaTime * speed * verticalInput);
+
+
+        //Restricciones límites de pantalla
+        // if player position y > 0
+        // set the position to x = x Current_position y = 0
+        //...
+
+        if (transform.position.y > 0)
+        {
+            transform.position = new Vector3(transform.position.x, 0, 0);
+        }
+        else if (transform.position.y < -4.2f)
+        {
+            transform.position = new Vector3(transform.position.x, -4.2f, 0);
+        }
+
+        if (transform.position.x < -8.2f)
+        {
+            transform.position = new Vector3(-8.2f, transform.position.y, 0);
+        }
+        else if (transform.position.x > 8.2f)
+        {
+            transform.position = new Vector3(8.2f, transform.position.y, 0);
+        }
     }
 }
